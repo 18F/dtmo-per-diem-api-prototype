b2c3250926ad0012ebbb71b2f3b045ae8fe8329e
Matt Henry <matt.henry@gsa.gov>
2019-10-21 16:52:50 -0400

    add url params/query strings for location searches

--- src/public/index.php
+++ src/public/index.php
@@ -13,13 +13,54 @@ function parseDataFile($file){
   return array_map("str_getcsv", explode(PHP_EOL, $data));
 }
 
-$app->get('/perdiem/conus', function(Request $req, Response $resp, array $args){
-  $file = "../data/connow-20.txt";
-  $array = parseDataFile($file);
+function getDataFilePath($conusOconus, $incAll, $incMil){
+  $perDiemTypes = [
+    "conus" => [
+      "e_connow",   /* Includes all Cities, Towns, Counties, DOD Installations, and GSA Defined Per Diem Locations. */
+      "e_conusnm",  /* Excludes DOD Installations */
+      "connow",     /* Includes Counties, DOD Installations, and GSA Defined Per Diem Locations */
+      "conusnm",    /* Includes Counties and GSA Defined Per Diem Locations. Excludes DOD Installations */
+      "conusmil"    /* DOD Installations only */
+    ],
+    "oconus" => [
+      "oconus",     /* Includes Military Installation */
+      "oconusnm"    /* Excludes Military Installation */
+    ]
+  ];
+
+  if ($conusOconus == "conus") {
+    if ($incAll  && $incMil) $fn  = "e_connow";
+    if ($incAll  && !$incMil) $fn = "e_conusnm";
+    if (!$incAll && $incMil) $fn  = "connow";
+    if (!$incAll && !$incMil) $fn = "conusnm";
+  } else {
+    $fn = ($incMil ? "oconus" : "oconusnm");
+  }
+
+  $path = "../data/$fn-20.txt";
+
+  return $path;
+}
+
+$app->get('/perdiem/{conus_oconus}/{locality}/{fy_or_pub_date}',
+          function(Request $req, Response $resp, array $args){
+
+  $params = $req->getQueryParams();
+  $include_mil = $params["include_military"] ?? false;
+  $include_all = $params["include_all"] ?? false;
+  $path = getDataFilePath($args["conus_oconus"], $include_all, $include_mil);
+  $array = parseDataFile($path);
+  $loc = $args["locality"];
+
+  $records_in_location = array_filter($array, function($el) use ($loc){
+    return strtolower($el[0]) == $loc;
+  });
 
   $resp = $resp->withHeader('Content-type', 'application/json');
-  $resp = $resp->withJson($array);
+  $resp = $resp->withJson($records_in_location);
+
   return $resp;
 });
 
 $app->run();
+
